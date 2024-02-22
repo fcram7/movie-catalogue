793fabdf866e04b47871bd57d5fe0ffbd5fd3260
fachri achmad maulana <fachri57@yahoo.com>
2024-02-22 15:27:29 +0700

    added codeceptjs test

--- e2e/Liking_Movies.test.js
+++ e2e/Liking_Movies.test.js
@@ -32,3 +32,49 @@ Scenario('liking one movie', async ({ I }) => {
 
   assert.strictEqual(firstMovieTitle, likedMovieTitle);
 });
+
+Scenario('Searching movies', async ({ I }) => {
+  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+
+  I.amOnPage('/');
+
+  I.seeElement('.movie__title a');
+
+  const titles = [];
+
+  for (let i = 1; i <= 3; i++) {
+    I.click(locate('.movie__title a').at(i));
+
+    I.seeElement('#likeButton');
+    I.click('#likeButton');
+
+    // eslint-disable-next-line no-await-in-loop
+    titles.push(await I.grabTextFrom('.movie__title'));
+
+    I.amOnPage('/');
+  }
+
+  I.amOnPage('/#/like');
+  I.seeElement('#query');
+
+  const visibleLikedMovies = await I.grabNumberOfVisibleElements('.movie-item');
+
+  assert.strictEqual(titles.length, visibleLikedMovies);
+
+  const searchQuery = titles[1].substring(1, 3);
+
+  I.fillField('#query', searchQuery);
+  I.pressKey('Enter');
+
+  const matchingMovies = titles.filter((title) => title.indexOf(searchQuery) !== -1);
+  const visibleSearchLikedMovies = await I.grabNumberOfVisibleElements('.movie-item');
+
+  assert.strictEqual(matchingMovies.length, visibleSearchLikedMovies);
+
+  for (let i = 0; i < matchingMovies.length; i++) {
+    // eslint-disable-next-line no-await-in-loop
+    const visibleTitle = await I.grabTextFrom(locate('.movie__title').at(i + 1));
+
+    assert.strictEqual(matchingMovies[i], visibleTitle);
+  }
+});
